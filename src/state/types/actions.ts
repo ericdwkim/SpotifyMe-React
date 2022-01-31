8120189747ab4f90291fafe7a273a43e02fd7e3e
ericdwkim <ericdwkim.io@gmail.com>
2022-01-30 23:03:12 -0500

    undoing prior eedits

--- src/state/types/actions.ts
+++ src/state/types/actions.ts
@@ -1,9 +1,9 @@
 import { Account } from './Account';
-import { Account } from './index'
+// import { Account } from './index'
 
 interface CreateAction {
   type: Account.ADD_ACCOUNT;
-  payload: Array; // must contain account_id, username, user_email, user_password (hashed?)
+  payload: Array<Account>; // must contain account_id, username, user_email, user_password (hashed?)
 }
 
 interface DeleteAction {
